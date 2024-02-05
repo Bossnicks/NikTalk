e455c8068f099947140e3bb49142cbf64b896513
Bossnicks <nikon.chigoya63@gmail.com>
2024-02-05 10:13:10 +0300

    visual representation
    creating a template for displaying a button for editing a text message and not displaying this button for other types of messages

---
+++ TemplateSelector/DeleteUpdateTemplateSelector.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using WpfApp1.MVVM.Model;
+
+namespace WpfApp1.TemplateSelector
+{
+    public class DeleteUpdateTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate TextTemplateUP { get; set; }
+        public DataTemplate ImageTemplateUP { get; set; }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            if (item is MessageModel message)
+            {
+                if (message.TypeOfMessage == "Text" && TextTemplateUP != null)
+                {
+                    return TextTemplateUP;
+                }
+                else if (message.TypeOfMessage == "Image" && ImageTemplateUP != null)
+                {
+                    return ImageTemplateUP;
+                }
+            }
+
+            // If no suitable template is found, return a default template or null
+            return base.SelectTemplate(item, container);
+        }
+    }
+}
